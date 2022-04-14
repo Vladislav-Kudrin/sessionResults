177fb869c819ea79944926feb6ffa59d4400d812
Vladislav <vladislav2001vlad26@gmail.com>
2022-04-14 13:51:36 +0300

    Added database config.

---
+++ src/sample/DBConfig.java
@@ -0,0 +1,25 @@
+package sample;
+
+class DBConfig {
+    static String host = "localhost";
+    static String port = "1433";
+    static String user = "root";
+    static String password = "****";
+    static String database = "SESSION_RESULTS";
+    static final String STUDENT = "STUDENT";
+    static final String MARKSHEET = "MARKSHEET";
+    static final String L_NAME = "L_NAME";
+    static final String F_NAME = "F_NAME";
+    static final String PATRONYMIC = "PATRONYMIC";
+    static final String G_INDEX = "G_INDEX";
+    static final String GENDER = "GENDER";
+    static final String M_STATUS = "M_STATUS";
+    static final String SUBJECT = "SUBJECT";
+    static final String CODE = "CODE";
+    static final String SUBJECT_CODE = "SUBJECT.CODE";
+    static final String GRADE = "GRADE";
+    static final String ID = "ID";
+    static final String GRADE_CODE = "GRADE.CODE";
+    static final String MARK = "MARK";
+    static final String IS_EXAM = "IS_EXAM";
+}
